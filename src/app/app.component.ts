768a7c34c5a93ffa0292d5fff19a27a23361053b
Sarah Angel <sangel217@hotmail.com>
2019-06-14 15:08:33 -0700

    fix date syntax in hardcoded constructor list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Oscar } from './models/oscar.models';
+import { Article } from './models/article.models';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +15,16 @@ export class AppComponent {
   hours: number = Math.floor((this.distance%(1000 * 60 * 60 * 24))/ (1000 * 60 * 60));
   minutes: number = Math.floor((this.distance % (1000 * 60 * 60)) / (1000 * 60));
   seconds: number = Math.floor((this.distance % (1000 * 60)) / 1000);
+
+  masterOscarList: Oscar[] = [
+    new Oscar('Best Picture', 'Green Book', 'Jim Burke', new Date("Feb, 24,2019, 20:00:00 EST")),
+    new Oscar('Best Actress', 'The Favourite', 'Olivia Colman', new Date("Feb, 24,2019, 20:00:00 EST")),
+    new Oscar('Best Documentary Short', 'Period. End of Sentence', 'Rayka Zehtabchi and Melissa Berton', new Date("Feb 24,2019 20:00:00 EST"))
+  ];
+
+  masterArticleList: Article[] = [
+    new Article('Oscars 2019 Highlights', ' Jamie Wylie', 'The 91st Oscars has come to a close...'),
+    new Article('Oscars 2019: Green Book Wins Best Picture', 'Jamie Wylie', 'Green Book is the 2019 Oscar winner...'),
+    new Article('Oscars in Memoriam 2019', 'Amy Barton', "Once again, The Oscars took time to...")
+  ];
 }
