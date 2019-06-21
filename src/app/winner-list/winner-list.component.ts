e62cfa1e34030fea9256420964b70020b0359391
Student <example@example.com>
2019-06-21 15:23:18 -0700

    add edit tag to winner list to be in admin component

--- src/app/winner-list/winner-list.component.ts
+++ src/app/winner-list/winner-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Oscar } from '../models/oscar.models';
 import { Router } from '@angular/router';
 import { OscarService } from '../oscar.service';
@@ -12,6 +12,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 export class WinnerListComponent implements OnInit {
   oscars: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private oscarService: OscarService) {}
 
@@ -19,7 +20,7 @@ export class WinnerListComponent implements OnInit {
     this.oscars = this.oscarService.getOscars();
   }
 
-  goToDetailPage(clickedWinner: Oscar){
+  goToDetailPage(clickedWinner){
     this.router.navigate(['oscars', clickedWinner.$key])
   }
 }
