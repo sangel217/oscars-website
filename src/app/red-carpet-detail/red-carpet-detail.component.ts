dc7c8e24d108dfd35116788c19d031fbe0d9dc2a
Student <example@example.com>
2019-06-21 12:48:28 -0700

    add to red carpet and red carpet detail to be able to display info from database

--- src/app/red-carpet-detail/red-carpet-detail.component.ts
+++ src/app/red-carpet-detail/red-carpet-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { RedCarpetService } from '../red-carpet.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-red-carpet-detail',
   templateUrl: './red-carpet-detail.component.html',
-  styleUrls: ['./red-carpet-detail.component.css']
+  styleUrls: ['./red-carpet-detail.component.css'],
+  providers: [RedCarpetService]
 })
 export class RedCarpetDetailComponent implements OnInit {
+  redCarpetId: string;
+  redCarpetToDisplay;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private redCarpetService: RedCarpetService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.redCarpetId = urlParameters['id'];
+    });
+    this.redCarpetToDisplay = this.redCarpetService.getRedCarpetById(this.redCarpetId);
   }
 
 }
--- src/app/red-carpet/red-carpet.component.ts
+++ src/app/red-carpet/red-carpet.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { RedCarpetService } from '../red-carpet.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import { RedCarpet } from '../models/red-carpet.models';
 
 @Component({
   selector: 'app-red-carpet',
   templateUrl: './red-carpet.component.html',
-  styleUrls: ['./red-carpet.component.css']
+  styleUrls: ['./red-carpet.component.css'],
+  providers: [RedCarpetService]
 })
 export class RedCarpetComponent implements OnInit {
+  redCarpets: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private redCarpetService: RedCarpetService) { }
 
   ngOnInit() {
+    this.redCarpets = this.redCarpetService.getRedCarpets();
+  }
+
+  goToDetailPage(clickedRedCarpet: RedCarpet){
+    // this.router.navigate(['redCarpets', clickedRedCarpet.$key])
   }
 
 }
